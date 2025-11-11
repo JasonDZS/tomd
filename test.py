c36a137a06d80813f663794442780de0d7a0828a
dizs <dizs@using.ai>
2025-11-11 20:49:08 +0800

    feat: enhance LLM prompt for improved markdown content quality and clarity

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from tomd import tomd
 print(
     tomd(
-        "https://aws.amazon.com/cn/blogs/china/agentive-ai-infrastructure-practice-series-1/",
-        llm_enhance=False,
+        "https://openai.com/index/chatgpt-for-veterans/",
+        llm_enhance=True,
         language="Chinese",
         # use_browser=True,
         # content_selector="div.blog-content",
